dc309a9b685c2e2593aea8a6348eaaa33003e2e4
Aurelien <aure.dugue@gmail.com>
2024-10-16 17:30:32 +0200

    a

--- Assets/Script/FinishLevelUI.cs
+++ Assets/Script/FinishLevelUI.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class FinishLevelUI : MonoBehaviour
 {
@@ -11,7 +13,7 @@ public class FinishLevelUI : MonoBehaviour
 
     private void OnEnable()
     {
-        
+        StartCoroutine(EndAnimation());
     }
 
     private IEnumerator EndAnimation()
@@ -19,11 +21,31 @@ public class FinishLevelUI : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         for (int i = 0; i < stars.Count; i++)
         {
-            if (!(PlayerPrefs.GetInt(levelName + "Star" + i) == 1))
-                yield return null;
-            stars[i].GetComponent<SpriteRenderer>().sprite = ownedStarSprite;
+            if (!(PlayerPrefs.GetInt(levelName + "Star" + i) != 1))
+                break;
+            stars[i].GetComponent<Image>().sprite = ownedStarSprite;
             yield return new WaitForSeconds(0.5f);
         }
+        yield return new WaitForSeconds(0.5f);
+        buttonsParent.SetActive(true);
+    }
+
+    public void NextLevel()
+    {
+        //SceneManager.LoadScene();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void SelectLevel()
+    {
+        SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Additive);
+        GameObject UI = GameObject.Find("Menu");
+        UI.GetComponent<RectTransform>().anchoredPosition = new Vector2(1920, 0);
+        //SceneManager.UnloadSceneAsync(levelName);
     }
 
 }
